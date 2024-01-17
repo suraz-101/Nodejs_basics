3d99c79c9abb5f3ff04389da28ec91d6183161f7
Suraj Pandey <surazpandey101@gmail.com>
2024-01-17 09:30:55 +0545

    practicing questions

---
+++ practice.js
@@ -0,0 +1,10 @@
+// Write a function to add two numbers
+
+const addTwoNumbers = (a, b) => a + b;
+
+console.log(addTwoNumbers(2, 3));
+
+// Write a js function to that check username and pasword
+//use bootstrap for login page design
+//set username and password as your email
+// Result alert you have logged in
