dff4c9f9cfc081dcd8f7a730b0768fe9a7e3a9a7
Suraj Pandey <surazpandey101@gmail.com>
2024-01-21 20:56:10 +0545

    using bcrypt package of npm to hash password

--- usingNpmModule/encryption/index.js
+++ usingNpmModule/encryption/index.js
@@ -1,5 +1,27 @@
-// const encryption = require("bcryptjs");
-// const password = "suraj";
+const encryption = require("bcryptjs");
+const password = "suraj12";
 
-// const res = encryption(password);
+// generating hash of the password ready to store in database
+// const hashing = (password) => {
+encryption.genSalt(10, (err, salt) => {
+  encryption.hash(password, salt, function (err, hash) {
+    console.log("The hashed password is :" + hash);
+    // return hash;
+    //   return hash;
+  });
+});
+// };
+
+//comparing technique
+// const compare = (hashp) => {
+//   encryption.compare("suraj123@", hashp, function (err, result) {
+//     {
+//       return result;
+//     }
+//   });
+// };
+
+// const hasp = hashing(password);
+// const res = compare(hasp);
+// console.log("hash password is : " + hasp);
 // console.log(res);
