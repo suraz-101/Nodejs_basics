8521db1919ffca7b6243e92baac15de059ca9116
Suraj Pandey <surazpandey101@gmail.com>
2024-01-22 22:28:50 +0545

    modifying encryption fie

--- usingNpmModule/index.js
+++ usingNpmModule/index.js
@@ -1,5 +1,5 @@
 const properUpperCase = require("proper-upper-case");
-const hashing = require("./encryption");
+const hashing = require("./encryption/index");
 
 const string = "surajPandey";
 
@@ -9,9 +9,10 @@ const converToProperUpperCase = (string) => {
 
 const res = converToProperUpperCase(string);
 // console.log(res);
+const password = "suraj12";
 
-const hashPass = hashing.dataEncryption(string);
-const result = hashing.comparePassword("suraj", hashPass);
+const hashPass = hashing.dataEncryption(password);
+const result = hashing.comparePassword("suraj12", hashPass);
 const result1 = (result) =>
   result ? "The password is correct" : "The password is incorrect";
 console.log(result1(result));
